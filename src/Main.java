b7e1b1b12994cbfc8069bef247c771ecbcba6a5b
SamsonPoon147 <144088882+SamsonPoon147@users.noreply.github.com>
2023-11-06 04:12:47 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
-
-    /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
-     Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String input) {
+        int count = 0;
+        String vowels = "aeiou";
+        for (int i = 0; i < input.length(); i++) {
+            char c = Character.toLowerCase(input.charAt(i));
+            if (vowels.indexOf(c) != -1) {
+                count++;
+            }
+            } return count;
+        }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,7 +20,16 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int notDivisibleBy235(int N) {
+        int count = 0;
 
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -24,6 +39,21 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe(String input) {
+        StringBuilder result = new StringBuilder();
+        boolean capitalizeNext = false;
 
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
 
+            if (currentChar == '-' || currentChar == '_') {
+                capitalizeNext = true;
+            } else if (capitalizeNext) {
+                result.append(Character.toUpperCase(currentChar));
+                capitalizeNext = false;
+            } else {
+                result.append(currentChar);
+            }
+        } return result.toString();
+    }
 }
